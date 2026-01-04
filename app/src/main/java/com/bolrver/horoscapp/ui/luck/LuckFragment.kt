e015359d254e6c5b615d6657d229b86e4cffe3b7
bolrver <bolrver@hotmail.com>
2026-01-04 22:43:14 +0000

    luck screen completed

--- app/src/main/java/com/bolrver/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/bolrver/horoscapp/ui/luck/LuckFragment.kt
@@ -1,6 +1,8 @@
 package com.bolrver.horoscapp.ui.luck
 
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,13 +16,19 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.bolrver.horoscapp.R
 import com.bolrver.horoscapp.databinding.FragmentLuckBinding
+import com.bolrver.horoscapp.ui.core.listeners.OnSwipeTouchListener
+import com.bolrver.horoscapp.ui.providers.RandomCardProvider
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Random
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
+
+    @Inject
+    lateinit var randomCardProvider : RandomCardProvider
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -28,11 +36,41 @@ class LuckFragment : Fragment() {
     }
 
     private fun initUI() {
+        preparePrediction()
         initListeners()
     }
 
+    private fun preparePrediction() {
+        val currentLuck = randomCardProvider.getLucky()
+        currentLuck?.let {luck ->
+            val currentPrediction = getString(luck.text)
+            binding.tvLucky.text = currentPrediction
+            binding.ivLuckyCard.setImageResource(luck.image)
+            binding.tvShare.setOnClickListener { shareResult(currentPrediction) }
+        }
+    }
+
+    private fun shareResult(prediction: String) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, prediction)
+            type = "text/plain"
+        }
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+    @SuppressLint("ClickableViewAccessibility")
     private fun initListeners() {
-        binding.ivRulette.setOnClickListener { spinRoulette() }
+        binding.ivRoulette.setOnClickListener { spinRoulette() }
+
+        binding.ivRoulette.setOnTouchListener(object : OnSwipeTouchListener(requireContext()){
+            override fun onSwipeRight() {
+                spinRoulette()
+            }
+            override fun onSwipeLeft() {
+                spinRoulette()
+            }
+        })
     }
 
     val random = Random()
@@ -40,7 +78,7 @@ class LuckFragment : Fragment() {
 
     private fun LuckFragment.spinRoulette() {
         val animator =
-            ObjectAnimator.ofFloat(binding.ivRulette, View.ROTATION, 0f, degrees.toFloat())
+            ObjectAnimator.ofFloat(binding.ivRoulette, View.ROTATION, 0f, degrees.toFloat())
         animator.duration = 2000
         animator.interpolator = DecelerateInterpolator()
         animator.doOnEnd { slideCard() }
